fd5e05b9631f92d9fe48db523a00b41025bf618f
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2026-04-30 07:00:55 +0000

    secondo commit

---
+++ progetto/src/app/fruits-component/fruits-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fruits-component',
+  imports: [],
+  templateUrl: './fruits-component.html',
+  styleUrl: './fruits-component.css',
+})
+export class FruitsComponent {}
