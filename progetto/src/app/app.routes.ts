16e20352718101f8a6f5e9337bcdf9c64b202819
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2026-05-04 10:29:27 +0000

    terzo commit

--- progetto/src/app/app.routes.ts
+++ progetto/src/app/app.routes.ts
@@ -6,3 +6,4 @@ export const routes: Routes = [
   { path: 'animals', component: AnimalComponent },
   { path: 'fruits', component: FruitsComponent }
 ];
+
--- progetto/src/app/app.ts
+++ progetto/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { AnimalComponent } from './animal-component/animal-component';
+import { FruitsComponent } from './fruits-component/fruits-component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AnimalComponent, FruitsComponent, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
